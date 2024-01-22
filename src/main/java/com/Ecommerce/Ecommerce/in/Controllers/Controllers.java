4ecf92fb84f3201fdacbd1f9e21c57880cdd7d89
AdnanAhmedchanna <adnanahmedtl601@gmail.com>
2024-01-22 14:47:13 +0500

    adding cart

--- src/main/java/com/Ecommerce/Ecommerce/in/Controllers/Controllers.java
+++ src/main/java/com/Ecommerce/Ecommerce/in/Controllers/Controllers.java
@@ -4,11 +4,9 @@ package com.Ecommerce.Ecommerce.in.Controllers;
 import com.Ecommerce.Ecommerce.in.Entitiy.Products;
 import com.Ecommerce.Ecommerce.in.Repo.Repo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Provider;
 import java.util.Optional;
@@ -38,6 +36,17 @@ public class Controllers {
 }
     @RestController("/cart")
     public class CartController{
+
+        @PutMapping
+        public ResponseEntity<String> saveProducts(@RequestBody Products products){
+            try {
+                repo.save(products);
+                return ResponseEntity.ok("Product saved succesasfully");
+            }
+            catch (Exception e){
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal error occured");
+            }
+        }
 
     }
 }
